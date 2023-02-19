92396fc3ca8be1ba7e54ecd939951590ccb21ee1
NImboNagatory <lukanogga@gmail.com>
2023-02-19 20:02:10 +0400

    d

--- Clculate hipotenuse.py
+++ Clculate hipotenuse.py
@@ -1,17 +1,19 @@
 from math import sqrt
 
-usr_in1 = int(input("Input first leg of Triangle >>>"))
-usr_in2 = int(input("Input second leg of Triangle >>>"))
+usr_in1 = input("Input first leg of Triangle >>>")
+usr_in2 = input("Input second leg of Triangle >>>")
 
 
 def calc_hipot(lg1, lg2):
     try:
+        lg1 = float(lg1)
+        lg2 = float(lg2)
         hipot = sqrt((lg1 * lg1) + (lg2 * lg2))
         s = (lg1+lg2+hipot)/2
         area = sqrt(s*((s-lg1)*(s-lg2)*(s-hipot)))
-        print(f"Triangle with legs of {lg1} and {lg2} the hypotenuse is {round(hipot)}")
-        print(f"Triangle with legs of {lg1} and {lg2} the area is {round(area)}")
-    except TypeError:
+        print(f"Triangle with legs of {lg1} and {lg2} the hypotenuse is {round(hipot, 2)}")
+        print(f"Triangle with legs of {lg1} and {lg2} the area is {round(area, 2)}")
+    except ValueError:
         print("Bad input")
 
 
